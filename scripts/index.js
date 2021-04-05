f3a15cea05582c778ec08c52e3224f63a045d76f
pramehta <pramehta@adobe.com>
2021-04-05 16:59:47 +0530

    Added UI for Create Post Modal

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+// Get the modal
+var createPostModal = document.getElementById("createPostModal");
+
+// Get the button to open the respective modal
+var createPostBtn  = document.getElementById("createPostBtn");
+
+// Open the modal on click
+createPostBtn.onclick = function() {
+  createPostModal.style.display = "block";
+}
