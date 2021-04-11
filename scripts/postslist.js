c5e282340b162f47a2c46be67a25a7b97a65d9a0
pramehta <pramehta@adobe.com>
2021-04-11 08:11:48 +0530

    Adding Close functionality for Delete Modal

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,7 +1,20 @@
 var deletePostModal = document.getElementById("deletePostModal");
 var deleteIcon = document.getElementById("delete1");
+var cancelDelete = document.getElementById("cancelDelete");
+var confirmDelete = document.getElementById("confirmDelete");
 
 // Open the modal on click
 deleteIcon.onclick = function() {
   deletePostModal.style.display = "block";
+}
+
+cancelDelete.onclick = function() {
+  deletePostModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == deletePostModal) {
+    deletePostModal.style.display = "none";
+  }
 }
